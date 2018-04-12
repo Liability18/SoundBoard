2eb8f5fb22e25e8a8e13a0142ce8434025c7607c
Corry Wright <s00182102@e2019-03.campus.itsligo.ie>
2018-04-12 17:38:38 +0100

    Hello

---
+++ SoundSensor.ino
@@ -0,0 +1,44 @@
+/*
+  AnalogReadSerial
+
+  Reads an analog input on pin 0, prints the result to the Serial Monitor.
+  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/AnalogReadSerial
+*/
+const int ledPin =  8;
+const int ledPinRed =  7;
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPinRed, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // print out the value you read:
+  Serial.println(sensorValue);
+  delay(50);
+  
+   if (sensorValue > 200) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+   if (sensorValue < 200) {
+    // turn LED on:
+    digitalWrite(ledPinRed, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPinRed, LOW);
+  }// delay in between reads for stability
+}
