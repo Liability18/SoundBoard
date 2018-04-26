38ea13325747f1484d32107673e7adb546c7d6a4
Liability18 <36045522+Liability18@users.noreply.github.com>
2018-04-26 16:56:31 +0100

    Add files via upload

--- SoundSensor.ino
+++ SoundSensor.ino
@@ -9,23 +9,28 @@
 
   http://www.arduino.cc/en/Tutorial/AnalogReadSerial
 */
+char devid[] = "v063F79B82775723";
 const int ledPin =  8;
 const int ledPinRed =  7;
+const int ledPinBlue =  6;
+
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
   pinMode(ledPinRed, OUTPUT);
+  pinMode(ledPinBlue, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
+  int
+  sensorValue = analogRead(A0);
   // print out the value you read:
   Serial.println(sensorValue);
-  delay(50);
+  delay(750);
   
    if (sensorValue > 200) {
     // turn LED on:
@@ -41,4 +46,11 @@ void loop() {
     // turn LED off:
     digitalWrite(ledPinRed, LOW);
   }// delay in between reads for stability
+  if (sensorValue < 75) {
+    // turn LED on:
+    digitalWrite(ledPinBlue, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPinBlue, LOW);
+}
 }
